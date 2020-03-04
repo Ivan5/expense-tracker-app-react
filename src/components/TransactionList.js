75a261cf356c4a6d75bc54e46d04402ff9d0151e
Ivan5 <converse_ivan@hotmail.com>
2020-03-04 16:13:34 -0600

    Make a delete action and transaction

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transacion } from "./Transacion";
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">X</button>
-        </li>
+        {transactions.map(transaction => (
+          <Transacion key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
